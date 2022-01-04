aac176ab84035e8eea573e30c3c0f3f97eebf879
msnkr <mikyle.designs@gmail.com>
2022-01-04 17:45:17 +0200

    .

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from tpblite import TPB
 from simple_term_menu import TerminalMenu
 
 
-
 def main():
-    li = []
     t = TPB()
     search = input('What do you want? ')
-    torrents = t.search(search)
-    item = [str(torrents)]
-    terminal_menu = TerminalMenu(item)
-    menu_entry_index = terminal_menu.show()
+    torrents = t.search(search).getBestTorrent()
+    return stream(torrents)
 
+def stream(torrents):
+    pass
+
+
+    
 if __name__ == "__main__":
     main()
-main()
