f2619e2176f3914388a40d24bedc192024fc389b
msnkr <mikyle.designs@gmail.com>
2022-01-07 00:07:20 +0200

    .

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import os
 def main():
     t = TPB()
     search = input('What do you want? ')
-    torrents = t.search(search).getBestTorrent()
-    torrent = torrents.magnetlink
-    run_webtorrent(torrent)
+    torrents = t.search(search)
+    for num, torrent in enumerate(torrents, start=1):
+        print(num, torrent)
 
-def run_webtorrent(torrent):
-    cmd = f'webtorrent "{torrent}" --mpv --player-args="video-on-top"'
-    os.system(cmd)
-
+    answer = int(input('Select which one you want to watch: '))
 
     
+# def run_webtorrent(torrent):
+#     cmd = f'webtorrent "{torrent}" --mpv --player-args="video-on-top"'
+#     os.system(cmd)
+
+
 if __name__ == "__main__":
     main()
