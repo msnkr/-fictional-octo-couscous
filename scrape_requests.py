991810e5e8f3c67e719860e77335f6539f2c872e
msnkr <mikyle.designs@gmail.com>
2022-01-07 01:09:32 +0200

    .

--- scrape_requests.py
+++ scrape_requests.py
@@ -1,3 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+url = 'https://1337x.to/'
+response = requests.get(site)
+if response.ok:
+    print('All Good')
+else:
+    print('Something is wrong!')
