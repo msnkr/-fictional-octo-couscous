8f9b018fc283ee2761f36b57f80e1ee9bcd9b47f
msnkr <mikyle.designs@gmail.com>
2022-01-15 15:22:40 +0200

    .

--- scrape_requests.py
+++ scrape_requests.py
@@ -1,13 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import pprint
 
-
-url = 'https://1337x.to/'
-response = requests.get(url)
-
-if response.ok:
-    print('Server is running')
-else:
-    print('Error with server')
-
-
+html = 'https://1337x.to/torrent/5092042/Resident-Evil-Welcome-to-Raccoon-City-2021-1080p-AMZN-WEBRip-DDP5-1-x264-CM/'
+response = requests.get(html)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.div['content-page'])
