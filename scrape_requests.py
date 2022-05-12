6c2e8866aa4e49210ba8445e24425b4102b07719
msnkr <mikyle.designs@gmail.com>
2022-05-12 10:53:47 +0200

    .

--- scrape_requests.py
+++ scrape_requests.py
@@ -1,9 +1,26 @@
-from tpb import TPB
-from tpb import CATEGORIES
+from tpblite import TPB
+from tpblite import CATEGORIES
+
+torrent_dict = {}
+accu = 0
+t = TPB('https://tpb.party')
+search = input('What do you want to watch?: \n Movies or Series? ').lower()
 
 
-t = TPB('https://thepiratebay.org')
-search = t.search('recipe book', category=CATEGORIES.VIDEO.MOVIES)
+if search == 'movies':
+    movie_name = input('What Movie?: ')
+    get_torrent = t.search(movie_name, category=CATEGORIES.VIDEO.MOVIES)
+elif search == 'series':
+    series_name = input('What series?: ')
+    get_torrent = t.search(series_name, category=CATEGORIES.VIDEO.TV_SHOWS)
 
-for torrent in t.search('resident evil'):
-    print(torrent.info)
+
+for title in get_torrent:
+    torrent_dict[accu] = title.title
+    accu += 1
+
+for item in torrent_dict:
+    print(f'{item}: {torrent_dict[item]}')
+
+select_dict = int(input('Select a number: '))
+
