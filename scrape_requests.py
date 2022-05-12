caa1b75c30e3cba9e1501ef6c56a379e281f0846
msnkr <mikyle.designs@gmail.com>
2022-05-12 09:24:17 +0200

    .

--- scrape_requests.py
+++ scrape_requests.py
@@ -1,3 +1,9 @@
-from bs4 import BeautifulSoup
-import requests
-import pprint
+from tpb import TPB
+from tpb import CATEGORIES
+
+
+t = TPB('https://thepiratebay.org')
+search = t.search('recipe book', category=CATEGORIES.VIDEO.MOVIES)
+
+for torrent in t.search('resident evil'):
+    print(torrent.info)
