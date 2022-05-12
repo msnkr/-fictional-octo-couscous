77a073366ad814a9073ddcab89b024c89ad5b509
msnkr <mikyle.designs@gmail.com>
2022-05-12 11:48:30 +0200

    .

--- scrape_requests.py
+++ scrape_requests.py
@@ -1,22 +1,30 @@
 from tpblite import TPB
 from tpblite import CATEGORIES
+import random
+from termcolor import colored
+
+colors = ['red', 'green', 'blue', 'yellow', 'grey', 'magenta', 'cyan']
+random_color = random.choice(colors)
 
 torrent_dict = {}
 magnet_dict = {}
 
 accu = 0
 t = TPB('https://tpb.party')
-search = input('What do you want to watch?: \n Movies or Series? ').lower()
+search = input(colored('What do you want to watch?: \n Movies or Series? ', random_color)).lower()
 
 
 if search == 'movies':
-    movie_name = input('What Movie?: ')
+    random_color = random.choice(colors)
+    movie_name = input(colored('What Movie?: ', random_color))
     get_torrent = t.search(movie_name, category=CATEGORIES.VIDEO.MOVIES)
+
 elif search == 'series':
     series_name = input('What series?: ')
     season = input('What Season?: ')
     episode = input('What Epsidode?: ')
     series = f'{series_name} s0{season} e0{episode}'
+    
     print(series)
     get_torrent = t.search(series, category=CATEGORIES.VIDEO.TV_SHOWS)
 
@@ -27,8 +35,10 @@ for title in get_torrent:
     accu += 1
 
 for item in torrent_dict:
-    print(f'{item}: {torrent_dict[item]}')
+    random_color = random.choice(colors)
+    print(f'{colored(item, random_color)}: {colored(torrent_dict[item], random_color)}')
 
-select_dict = int(input('Select a number: '))
+random_color = random.choice(colors)
+select_dict = int(input(colored('Select a number: ', random_color)))
 torrent_link = magnet_dict[select_dict]
 
