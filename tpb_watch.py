fab3f5895919fa2626af06112e38a80f71401d92
msnkr <mikyle.designs@gmail.com>
2022-05-18 22:55:47 +0200

    cleaned code

--- tpb_watch.py
+++ tpb_watch.py
@@ -12,47 +12,47 @@ def change_colors():
         return random.choice(colors)
 
 
-while start_again:
+def play_torrent(torrent_dict, magnet_dict):
+    if torrent_dict != True:
+        print(colored('Theres nothing to show...', change_colors()))
+        search_torrent()
+    else:
+        for item in torrent_dict:
+            print(f"| {colored(item, change_colors())} | {colored(torrent_dict[item], change_colors())} |")
+
+    select_dict = int(input(colored('Select a number: ', change_colors())))
+    torrent_itself = magnet_dict[select_dict]
+
+    subprocess.call(['webtorrent', torrent_itself, '--mpv'])
+
+
+def search_torrent():
     torrent_dict = {}
     magnet_dict = {}
+    accu = 1
 
-    accu = 0
     t = TPB('https://tpb.party')
-    search = input(colored('What do you want to watch?: \n (M)ovie/(S)eries ', change_colors())).lower()
-
+    search = input(colored('What do you want to watch? A (M)ovie or (S)eries?: ', change_colors())).lower()
 
     if search == 'm':
-        movie_name = input(colored('What Movie?: ', change_colors()))
+        movie_name = input(colored('Whats the movie name?: ', change_colors())).lower()
         get_torrent = t.search(movie_name, category=CATEGORIES.VIDEO.MOVIES)
 
     elif search == 's':
-        series_name = input('What series?: ')
-        season = input('What Season?: ')
-        episode = input('What Epsidode?: ')
-        series = f'{series_name} s0{season} e0{episode}'
-        
-        print(f'Looking for {colored(series, change_colors())}')
-        get_torrent = t.search(series, category=CATEGORIES.VIDEO.TV_SHOWS)
+        series_name = input(colored('Whats the series name?: ', change_colors())).lower()
+        series_season = int(input(colored('What Season?: ', change_colors())))
+        series_episode = int(input(colored('What Episode?: ', change_colors())))
+        full_series = f'{series_name} s0{series_season} e0{series_episode}'
 
-    if torrent_dict != True:
-        print('Theres nothing to display. ')
-        
+        get_torrent = t.search(full_series, category=CATEGORIES.VIDEO.TV_SHOWS)
+
 
     for title in get_torrent:
         torrent_dict[accu] = title.title
         magnet_dict[accu] = title.magnetlink
-        accu += 1
+        accu += 1 
 
-    for item in torrent_dict:
-        print(f'| {colored(item, change_colors())} | {colored(torrent_dict[item], change_colors())} |')
+    play_torrent(torrent_dict, magnet_dict)
 
 
-    select_dict = int(input(colored('Select a number: ', change_colors())))
-    torrent_link = magnet_dict[select_dict]
-
-    subprocess.call(['webtorrent', torrent_link, '--mpv'])
-
-    re_start_again = input('Are you done?: Y/N: ').lower()
-    if re_start_again == 'y':
-        print('Bye-Bye')
-        start_again = False
+search_torrent()
