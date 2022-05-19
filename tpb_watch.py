6eec411f55bf3a69225e7750470456bcba8d552f
msnkr <mikyle.designs@gmail.com>
2022-05-19 12:38:43 +0200

    Update tpb_watch.py

--- tpb_watch.py
+++ tpb_watch.py
@@ -5,6 +5,12 @@ import random
 from termcolor import colored
 
 
+def quit_program(key):
+    if key == 'q':
+        print('Bye-Bye!')
+        exit()
+
+
 def play_torrent(torrent_dict, magnet_dict):
     if bool(torrent_dict) != True:
         print('There was nothing to be found... Taking you back to main screen...')
@@ -13,7 +19,8 @@ def play_torrent(torrent_dict, magnet_dict):
         for item in torrent_dict:
             print(f"| {colored(item, change_colors())} | {colored(torrent_dict[item], change_colors())} |")
 
-    select_dict = int(input(colored('Select a number: ', change_colors())))
+    select_dict = input(colored('Select a number: ', change_colors()))
+    quit_program(select_dict)
     torrent_itself = magnet_dict[select_dict]
 
     # subprocess.call(['webtorrent', torrent_itself, '--mpv'])
@@ -27,18 +34,19 @@ def search_torrent():
     t = TPB('https://tpb.party')
     print('Press "q" to quit.')
     search = input(colored('What do you want to watch? A (M)ovie or (S)eries?: ', change_colors())).lower()
-    if search == 'q':
-        print('Bye-Bye!')
-        exit()
+    quit_program(search)
 
-    elif search == 'm':
+    if search == 'm':
         movie_name = input(colored('Whats the movie name?: ', change_colors())).lower()
         get_torrent = t.search(movie_name, category=CATEGORIES.VIDEO.MOVIES)
 
     elif search == 's':
         series_name = input(colored('Whats the series name?: ', change_colors())).lower()
-        series_season = int(input(colored('What Season?: ', change_colors())))
-        series_episode = int(input(colored('What Episode?: ', change_colors())))
+        quit_program(series_name)
+        series_season = input(colored('What Season?: ', change_colors()))
+        quit_program(series_season)
+        series_episode = input(colored('What Episode?: ', change_colors()))
+        quit_program(series_episode)
         full_series = f'{series_name} s0{series_season} e0{series_episode}'
 
         get_torrent = t.search(full_series, category=CATEGORIES.VIDEO.TV_SHOWS)
@@ -50,12 +58,6 @@ def search_torrent():
         accu += 1 
 
     play_torrent(torrent_dict, magnet_dict)
-
-    play_again = input(colored('Would you like to quit?: Y/N: ')).lower()
-    if play_again == 'y':
-        search_torrent()
-    else:
-        print('Bye-Bye')
 
 
 def change_colors():
